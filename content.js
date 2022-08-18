78b7168cbe8e64272fd4a7d3a0d978326c78ae71
Yuki Pietrowski <contact.yuwukii@gmail.com>
2022-08-18 16:08:57 +0200

    Added dancing gif and fixed content script

--- content.js
+++ content.js
@@ -1,2 +1,9 @@
-const image = document.createElement("img").src = "assests/images/NecoArc.png";
+var image = document.createElement('img');
+image.src ="https://raw.githubusercontent.com/yuwukii/NecoArcBuddy/master/assets/images/neco-arc-dance.gif"
+image.style.height = "200px";
+image.style.width = "auto";
+image.style.position = "fixed";
+image.style.bottom = "20px";
+image.style.right = "40px";
+image.style.zIndex = "9999";
 document.body.appendChild(image);
