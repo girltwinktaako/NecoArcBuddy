48aea61438b583b3e975b2ceb80bc34490034509
Yuki Pietrowski <contact.yuwukii@gmail.com>
2022-08-18 16:13:05 +0200

    Added background music

--- content.js
+++ content.js
@@ -4,6 +4,10 @@ image.style.height = "200px";
 image.style.width = "auto";
 image.style.position = "fixed";
 image.style.bottom = "20px";
-image.style.right = "40px";
+image.style.right = "20px";
 image.style.zIndex = "9999";
 document.body.appendChild(image);
+
+const music = new Audio('https://raw.githubusercontent.com/yuwukii/NecoArcBuddy/master/assets/musci/bgm.ogg');
+music.volume = 20;
+music.play();
